88a76fd0100c789130716c43908f90e3d0dd880a
Мартин Мухаремовић <makimuh02@gmail.com>
2024-05-16 16:42:35 +0200

    Added initial classes for Undo Actions

---
+++ NetworkService/NetworkService/NetworkService/Helpers/StyleConverters/UndoStyleConverter.cs
@@ -0,0 +1,32 @@
+﻿using NetworkService.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NetworkService.Helpers.StyleConverters
+{
+    public class UndoStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is ActionType actionType && actionType == ActionType.NoAction)
+            {
+                return Application.Current.FindResource("UndoButtonUnavailableStyle");
+            }
+            else
+            {
+                return Application.Current.FindResource("UndoButtonStyle");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetworkService/NetworkService/NetworkService/Helpers/UndoActionHolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Helpers
+{
+    public enum ActionType
+    {
+        NoAction = -1
+    }
+    public class UndoActionHolder
+    {
+        private ActionType actionId;
+
+        public UndoActionHolder()
+        {
+            actionId = ActionType.NoAction;
+        }
+
+        public ActionType ActionId
+        {
+            private set { }
+            get { return actionId; }
+        }
+    }
+}
