ab5b1ea96a674dd975a1969a90181cf4cac77031
Мартин Мухаремовић <makimuh02@gmail.com>
2024-06-05 23:32:11 +0200

    Static Resources and converter
    Added static resources needed to create back button and it's functionality.

---
+++ NetworkService/NetworkService/NetworkService/Helpers/StyleConverters/BackStyleConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NetworkService.Helpers.StyleConverters
+{
+    public class BackStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((value as BindableBase) == null)
+            {
+                return Application.Current.FindResource("BackButtonUnavailableStyle");
+            }
+            else
+            {
+                return Application.Current.FindResource("BackButtonStyle");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
